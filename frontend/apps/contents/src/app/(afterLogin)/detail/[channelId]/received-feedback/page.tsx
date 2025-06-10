1116cd4d32fb8593bdcbb33b7231d47161f9b52c
goawmfhfl <goawmfhfl1@naver.com>
2025-06-10 21:06:14 +0900

    feat: RequireAuth추가

--- frontend/apps/contents/src/app/(afterLogin)/detail/[channelId]/received-feedback/page.tsx
+++ frontend/apps/contents/src/app/(afterLogin)/detail/[channelId]/received-feedback/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReviewList from "./_components/ReviewList";
 import OwnerCookieSetter from "@util/hooks/OwnerCookieSetter";
+import RequireAuth from "@/app/_components/RequireAuth";
 
 export default function page() {
   return (
     <>
       <OwnerCookieSetter />
+      <RequireAuth />
       <ReviewList />;
     </>
   );
--- frontend/apps/contents/src/app/(afterLogin)/profile/page.tsx
+++ frontend/apps/contents/src/app/(afterLogin)/profile/page.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import HeaderConfigurator from "@ui/components/Header/HeaderConfigurator";
 import Profile from "./_components/Profile";
 import OwnerCookieSetter from "@util/hooks/OwnerCookieSetter";
+import RequireAuth from "@/app/_components/RequireAuth";
 
 export default function page() {
   return (
     <div>
       <OwnerCookieSetter />
+      <RequireAuth />
       <HeaderConfigurator
         config={{
           title: "프로필",
